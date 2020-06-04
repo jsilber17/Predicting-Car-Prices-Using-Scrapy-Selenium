59df1d785793bf028188a4173afc78e9ddba7bfd
Jacob Silber <jsilber17@gmail.com>
2020-06-04 17:39:46 -0600

    added comments to the script

--- scraper_project/carfax/carfax/spiders/craigslist_crawl.py
+++ scraper_project/carfax/carfax/spiders/craigslist_crawl.py
@@ -19,11 +19,21 @@ class AutoListSpider(scrapy.Spider):
         csv_number = 0
 
         def parse(self, response):
-            sel = Selector(response) 
+
+            ''' 
+            This function requests a url and recieves the HTML response as text
+            Once the script has received the text, it scrapes the car ad urls on the page
+            With all of the car ad urls, the script then requests each individual url and scrapes that page in the next function 
+            ''' 
+
+            sel = Selector(response)
+
+            # Iterates through the response HTML and extracting all of urls for car ads  
             for url in sel.xpath("//p[@class='result-info']/a/@href").extract(): 
+                # This yield statement then takes one url and "returns" it to parse_craigslist_ad to scrape the data for each car 
                 yield Request(url, callback=self.parse_craiglist_ad)
-            print(response.url.split('.')[0][8:])
-            print('fuckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
+            
+            # Find the next page url in the response HTML so we can navigate to the next page of each indiviual city 
             next_page = sel.xpath("//span[@class='buttons']/a[@class='button next']/@href").extract_first() 
             next_page = 'http://{}.craigslist.org'.format(response.url.split('.')[0][8:]) + str(next_page)
 
@@ -32,9 +42,21 @@ class AutoListSpider(scrapy.Spider):
 
         
         def parse_craiglist_ad(self, response):
+
+            '''
+            Recieves a url for an individual car ad
+            Scrapes the data and puts that data into a dataframe 
+            Moves onto the next page after it is finished scraping 
+            ''' 
+            
+            # Used as an ouput for the csvs 
             self.csv_number += 1 
+
             cols = []
+
             sel = Selector(response) 
+
+            # Data / columns being scraped and then put into a Pandas DataFrame 
             items = sel.xpath("//section[@id='postingbody']/text()").extract()
             items_2 = sel.xpath("//section[@id='postingbody']//strong").extract()
             important_info_fields = sel.xpath("//p[@class='attrgroup']//span/text()").extract()
@@ -61,9 +83,11 @@ class AutoListSpider(scrapy.Spider):
 
             df = pd.DataFrame(cols) 
             df.to_csv('car_data/out{}.csv'.format(self.csv_number))
-
+            
+            # Calls back to the first function and brings us to the next page of results to do more scraping 
             next_page = sel.xpath("//span[@class='buttons']/a[@class='button next']/@href").extract_first()
             next_page = "http://{}.craigslist.org".format(response.url.split('.')[0][8:]) + str(next_page)
+
             if next_page: 
                 yield Request(next_page, callback=self.parse)
 
